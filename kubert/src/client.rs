09ac761721e3f0f682061b92d155bcdc5820fe49
Oliver Gould <ver@buoyant.io>
2022-02-19 11:45:54 -0800

    Simplify dependencies (#2)
    Replace the `kube` dependency with `kube-client`, which is used only by
    the `client` feature.
    
    Update Cargo.toml to express patch versions, since compilation failed on
    minimal versions.

--- kubert/src/client.rs
+++ kubert/src/client.rs
@@ -1,4 +1,4 @@
-pub use kube::Client;
+pub use kube_client::Client;
 use thiserror::Error;
 
 // TODO configure a --kubeconfig
@@ -13,19 +13,19 @@ pub struct ClientArgs {
 #[derive(Debug, Error)]
 pub enum Error {
     #[error(transparent)]
-    Kubeconfig(#[from] kube::config::KubeconfigError),
+    Kubeconfig(#[from] kube_client::config::KubeconfigError),
 
     #[error(transparent)]
-    Client(#[from] kube::Error),
+    Client(#[from] kube_client::Error),
 }
 
 impl ClientArgs {
     pub async fn try_client(self) -> Result<Client, Error> {
-        let c = kube::config::KubeConfigOptions {
+        let c = kube_client::config::KubeConfigOptions {
             context: self.context,
             ..Default::default()
         };
-        kube::Config::from_kubeconfig(&c)
+        kube_client::Config::from_kubeconfig(&c)
             .await?
             .try_into()
             .map_err(Error::from)
