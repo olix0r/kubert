eb47605dd4bebf9fffa9da728454947de0e9f774
Oliver Gould <ver@buoyant.io>
2022-02-20 02:50:44 +0000

    Prepare v0.2.1

--- kubert/src/log.rs
+++ kubert/src/log.rs
@@ -8,6 +8,7 @@ pub use tracing_subscriber::EnvFilter;
 /// Configures whether logs should be emitted in plaintext (the default) or as JSON-encoded
 /// messages
 #[derive(Clone, Debug)]
+#[cfg_attr(docsrs, doc(cfg(any(feature = "log"))))]
 pub enum LogFormat {
     /// The default plaintext format
     Plain,
@@ -19,6 +20,7 @@ pub enum LogFormat {
 /// Indicates that an invalid log format was specified
 #[derive(Debug, Error)]
 #[error("invalid log level: {0} must be 'plain' or 'json'")]
+#[cfg_attr(docsrs, doc(cfg(any(feature = "log"))))]
 pub struct InvalidLogFormat(String);
 
 // === impl LogFormat ===
