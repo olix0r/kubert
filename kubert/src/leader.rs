abf0afc598a42da4a50e959b82cd041c6022a93a
Oliver Gould <ver@buoyant.io>
2022-10-18 22:54:46 +0000

    Rework with claim API
    Signed-off-by: Oliver Gould <ver@buoyant.io>

--- kubert/src/leader.rs
+++ kubert/src/leader.rs
@@ -1,16 +1,58 @@
 //! Uses leases to establish leadership among replicas.
 
-#![allow(warnings, missing_docs)]
+#![allow(missing_docs)]
 
 use k8s_openapi::{api::coordination::v1 as coordv1, apimachinery::pkg::apis::meta::v1::MicroTime};
-use tokio::time::{Duration, Instant};
+use tokio::time::Duration;
 
 pub struct Lease {
-    field_manager: String,
-    identity: String,
-    client: kube_client::Client,
-    namespace: String,
+    api: kube_client::Api<coordv1::Lease>,
     name: String,
+    field_manager: String,
+    state: tokio::sync::Mutex<State>,
+}
+
+#[derive(Clone, Debug)]
+pub struct ClaimParams {
+    /// The unique identity of the claimant
+    pub identity: String,
+
+    /// The duration of the lease
+    pub lease_duration: Duration,
+
+    /// The amount of time before the lease expiration that the lease holder
+    /// should renew the lease
+    pub renew_grace_period: Option<Duration>,
+}
+
+#[derive(Clone, Debug)]
+pub struct Claim {
+    pub holder: String,
+
+    /// The time that the lease expires
+    pub expiry: chrono::DateTime<chrono::Utc>,
+}
+
+/// Indicates an error interacting with the Lease API
+#[derive(Debug, thiserror::Error)]
+pub enum Error {
+    /// An error was received from the Kubernetes API
+    #[error("failed to get lease: {0}")]
+    Api(#[from] kube_client::Error),
+
+    /// Lease resource does not have a resourceVersion
+    #[error("lease does not have a resource version")]
+    MissingResourceVersion,
+
+    /// Lease resource does not have a spec
+    #[error("lease does not have a spec")]
+    MissingSpec,
+}
+
+#[derive(Clone, Debug)]
+struct State {
+    meta: Meta,
+    claim: Option<Claim>,
 }
 
 #[derive(Clone, Debug)]
@@ -19,226 +61,222 @@ struct Meta {
     transitions: u16,
 }
 
-enum State {
-    Held {
-        meta: Meta,
-        until: Instant,
-    },
-    HeldByAnother {
-        meta: Meta,
-        holder_id: String,
-        until: Instant,
-    },
-    Released(Meta),
+// === impl Claim ===
+
+impl Claim {
+    pub fn is_currently_held_by(&self, claimant: &str) -> bool {
+        self.holder == claimant && chrono::Utc::now() < self.expiry
+    }
+
+    pub async fn sleep_until_expiry(&self) {
+        self.sleep_until_before_expiry(Duration::ZERO).await;
+    }
+
+    pub async fn sleep_until_before_expiry(&self, grace: Duration) {
+        if let Ok(remaining) = (self.expiry - chrono::Utc::now()).to_std() {
+            let sleep = remaining.saturating_sub(grace);
+            if !sleep.is_zero() {
+                tokio::time::sleep(sleep).await;
+            }
+        }
+    }
 }
 
-#[derive(Debug, thiserror::Error)]
-pub enum Error {
-    #[error("failed to get lease: {0}")]
-    Api(#[from] kube_client::Error),
-
-    #[error("lease does not have a resource version")]
-    MissingResourceVersion,
-
-    #[error("lease does not have a spec")]
-    MissingSpec,
-}
+// === impl Lease ===
 
 impl Lease {
-    pub fn new(
-        field_manager: String,
-        identity: String,
-        client: kube_client::Client,
-        namespace: String,
+    pub async fn init(
+        api: kube_client::Api<coordv1::Lease>,
         name: String,
-    ) -> Self {
-        Self {
-            field_manager,
-            identity,
-            client,
-            namespace,
+        field_manager: String,
+    ) -> Result<Self, Error> {
+        let state = Self::sync(api.clone(), &*name).await?;
+        Ok(Self {
+            api,
             name,
+            field_manager,
+            state: tokio::sync::Mutex::new(state),
+        })
+    }
+
+    pub async fn claim(&self, params: &ClaimParams) -> Result<Claim, Error> {
+        let mut state = self.state.lock().await;
+        loop {
+            if let Some(claim) = state.claim.as_ref() {
+                let now = chrono::Utc::now();
+
+                if claim.holder == params.identity {
+                    let renew_at = claim.expiry
+                        - chrono::Duration::from_std(params.renew_grace_period.unwrap_or_default())
+                            .unwrap_or_else(|_| chrono::Duration::zero());
+                    if now < renew_at {
+                        return Ok(claim.clone());
+                    }
+
+                    match self.renew(&state.meta, params).await {
+                        Ok((claim, meta)) => {
+                            *state = State {
+                                meta,
+                                claim: Some(claim.clone()),
+                            };
+                            return Ok(claim);
+                        }
+                        Err(e) if Self::is_conflict(&e) => {
+                            *state = Self::sync(self.api.clone(), &*self.name).await?;
+                            continue;
+                        }
+                        Err(e) => return Err(e),
+                    }
+                }
+
+                debug_assert!(claim.holder != params.identity);
+                if now < claim.expiry {
+                    return Ok(claim.clone());
+                }
+            }
+
+            match self.acquire(&state.meta, params).await {
+                Ok((claim, meta)) => {
+                    *state = State {
+                        meta,
+                        claim: Some(claim.clone()),
+                    };
+                    return Ok(claim);
+                }
+                Err(e) if Self::is_conflict(&e) => {
+                    *state = Self::sync(self.api.clone(), &*self.name).await?;
+                }
+                Err(e) => return Err(e),
+            }
         }
     }
 
-    pub fn spawn(
-        self,
-        lease_duration: Duration,
-    ) -> (
-        tokio::task::JoinHandle<Result<(), Error>>,
-        tokio::sync::watch::Receiver<bool>,
-    ) {
-        let (tx, rx) = tokio::sync::watch::channel(false);
-        let task = tokio::spawn(async move {
-            let mut state = self.get_state().await?;
-            loop {
-                let res = match state {
-                    State::Held { meta, until } => {
-                        if tx.send(true).is_err() {
-                            return Ok(());
-                        }
-                        tokio::time::sleep_until(until - Duration::from_secs(3)).await;
-                        self.try_renew(lease_duration, meta).await
-                    }
-                    State::HeldByAnother { meta, until, .. } => {
-                        if tx.send(false).is_err() {
-                            return Ok(());
-                        }
-                        tokio::time::sleep_until(until).await;
-                        self.try_acquire(lease_duration, meta).await
-                    }
-                    State::Released(meta) => {
-                        if tx.send(false).is_err() {
-                            return Ok(());
-                        }
-                        self.try_acquire(lease_duration, meta).await
-                    }
-                };
-                state = match res {
-                    Ok(s) => s,
-                    Err(e) if e.is_conflict() => {
-                        tracing::warn!(error = ?e, "conflict while trying to acquire lease");
-                        self.get_state().await?
-                    }
-                    Err(e) => return Err(e),
-                };
-            }
-        });
-        (task, rx)
+    async fn acquire(&self, meta: &Meta, params: &ClaimParams) -> Result<(Claim, Meta), Error> {
+        let lease_duration = chrono::Duration::from_std(params.lease_duration)
+            .unwrap_or_else(|_| chrono::Duration::max_value());
+        let now = chrono::Utc::now();
+        let lease = self
+            .patch(serde_json::json!({
+                "metadata": {
+                    "resourceVersion": meta.version,
+                },
+                "spec": {
+                    "acquireTime": MicroTime(now),
+                    "renewTime": MicroTime(now),
+                    "holderIdentity": params.identity,
+                    "leaseDurationSeconds": lease_duration.num_seconds(),
+                    "leaseTransitions": meta.transitions + 1,
+                },
+            }))
+            .await?;
+
+        let claim = Claim {
+            holder: params.identity.clone(),
+            expiry: now + lease_duration,
+        };
+        let meta = Meta {
+            version: lease
+                .metadata
+                .resource_version
+                .ok_or(Error::MissingResourceVersion)?,
+            transitions: meta.transitions + 1,
+        };
+        Ok((claim, meta))
     }
 
-    async fn get_state(&self) -> Result<State, Error> {
-        let api =
-            kube_client::Api::<coordv1::Lease>::namespaced(self.client.clone(), &*self.namespace);
-        let lease = api.get(&*self.name).await?;
-        let version = lease
-            .metadata
-            .resource_version
-            .ok_or(Error::MissingResourceVersion)?;
+    async fn renew(&self, meta: &Meta, params: &ClaimParams) -> Result<(Claim, Meta), Error> {
+        let lease_duration = chrono::Duration::from_std(params.lease_duration)
+            .unwrap_or_else(|_| chrono::Duration::max_value());
+        let now = chrono::Utc::now();
+        let lease = self
+            .patch(serde_json::json!({
+                "metadata": {
+                    "resourceVersion": meta.version,
+                },
+                "spec": {
+                    "renewTime": MicroTime(now),
+                    "leaseDurationSeconds": lease_duration.num_seconds(),
+                },
+            }))
+            .await?;
+
+        let claim = Claim {
+            holder: params.identity.clone(),
+            expiry: now + lease_duration,
+        };
+        let meta = Meta {
+            version: lease
+                .metadata
+                .resource_version
+                .ok_or(Error::MissingResourceVersion)?,
+            transitions: meta.transitions,
+        };
+        Ok((claim, meta))
+    }
+
+    async fn patch<P>(&self, patch: P) -> Result<coordv1::Lease, kube_client::Error>
+    where
+        P: serde::Serialize + std::fmt::Debug,
+    {
+        tracing::debug!(?patch, "acquiring lease");
+        let params = kube_client::api::PatchParams {
+            field_manager: Some(self.field_manager.clone()),
+            ..Default::default()
+        };
+        self.api
+            .patch(&*self.name, &params, &kube_client::api::Patch::Apply(patch))
+            .await
+    }
+
+    async fn sync(api: kube_client::Api<coordv1::Lease>, name: &str) -> Result<State, Error> {
+        let lease = api.get(name).await?;
 
         let spec = match lease.spec {
             Some(spec) => spec,
             None => return Err(Error::MissingSpec),
         };
 
+        let version = lease
+            .metadata
+            .resource_version
+            .ok_or(Error::MissingResourceVersion)?;
         let transitions = spec.lease_transitions.unwrap_or(0).try_into().unwrap_or(0);
-
         let meta = Meta {
             version,
             transitions,
         };
 
-        macro_rules! or_released {
+        macro_rules! or_unclaimed {
             ($e:expr) => {
                 match $e {
                     Some(e) => e,
                     None => {
-                        return Ok(State::Released(meta));
+                        return Ok(State { meta, claim: None });
                     }
                 }
             };
         }
 
-        let holder_id = or_released!(spec.holder_identity);
+        let holder = or_unclaimed!(spec.holder_identity);
 
-        let MicroTime(renew_time) = or_released!(spec.renew_time);
+        let MicroTime(renew_time) = or_unclaimed!(spec.renew_time);
         let lease_duration =
-            chrono::Duration::seconds(or_released!(spec.lease_duration_seconds).into());
-        let released_at = renew_time + lease_duration;
-        let remaining = or_released!((released_at - chrono::Utc::now()).to_std().ok());
-        let until = Instant::now() + remaining;
-        if remaining.is_zero() {
-            return Ok(State::Released(meta));
+            chrono::Duration::seconds(or_unclaimed!(spec.lease_duration_seconds).into());
+        let expiry = renew_time + lease_duration;
+        if expiry <= chrono::Utc::now() {
+            return Ok(State { meta, claim: None });
         }
 
-        if holder_id == self.identity {
-            return Ok(State::Held { meta, until });
-        }
-
-        Ok(State::HeldByAnother {
+        Ok(State {
             meta,
-            holder_id,
-            until,
+            claim: Some(Claim { holder, expiry }),
         })
     }
 
-    async fn try_acquire(&self, duration: Duration, meta: Meta) -> Result<State, Error> {
-        let until = Instant::now() + duration;
-        let now = chrono::Utc::now();
-        let patch = serde_json::json!({
-            "metadata": {
-                "resourceVersion": meta.version,
-            },
-            "spec": {
-                "acquireTime": MicroTime(now),
-                "renewTime": MicroTime(now),
-                "holderIdentity": self.identity,
-                "leaseDurationSeconds": duration.as_secs(),
-                "leaseTransitions": meta.transitions + 1,
-            },
-        });
-        tracing::debug!(?patch, "acquiring lease");
-
-        let params = kube_client::api::PatchParams {
-            field_manager: Some(self.field_manager.clone()),
-            ..Default::default()
-        };
-        let lease =
-            kube_client::Api::<coordv1::Lease>::namespaced(self.client.clone(), &*self.namespace)
-                .patch(&*self.name, &params, &kube_client::api::Patch::Apply(patch))
-                .await?;
-        Ok(State::Held {
-            until,
-            meta: Meta {
-                version: lease
-                    .metadata
-                    .resource_version
-                    .ok_or(Error::MissingResourceVersion)?,
-                transitions: meta.transitions + 1,
-            },
-        })
-    }
-
-    async fn try_renew(&self, duration: Duration, meta: Meta) -> Result<State, Error> {
-        let until = Instant::now() + duration;
-        let now = chrono::Utc::now();
-        let patch = serde_json::json!({
-            "metadata": {
-                "resourceVersion": meta.version,
-            },
-            "spec": {
-                "renewTime": MicroTime(now),
-                "leaseDurationSeconds": duration.as_secs(),
-            },
-        });
-        tracing::debug!(?patch, "acquiring lease");
-
-        let params = kube_client::api::PatchParams {
-            field_manager: Some(self.field_manager.clone()),
-            ..Default::default()
-        };
-        let lease =
-            kube_client::Api::<coordv1::Lease>::namespaced(self.client.clone(), &*self.namespace)
-                .patch(&*self.name, &params, &kube_client::api::Patch::Apply(patch))
-                .await?;
-        Ok(State::Held {
-            until,
-            meta: Meta {
-                version: lease
-                    .metadata
-                    .resource_version
-                    .ok_or(Error::MissingResourceVersion)?,
-                ..meta
-            },
-        })
-    }
-}
-
-impl Error {
-    fn is_conflict(&self) -> bool {
+    fn is_conflict(err: &Error) -> bool {
         matches!(
-            self,
-            Self::Api(kube_client::Error::Api(kube_core::ErrorResponse { code, .. }))
+            err,
+            Error::Api(kube_client::Error::Api(kube_core::ErrorResponse { code, .. }))
                 if hyper::StatusCode::from_u16(*code).ok() == Some(hyper::StatusCode::CONFLICT)
         )
     }
