50a65689c0f31073319b560aa1ed36c079c5fa29
Eliza Weisman <eliza@buoyant.io>
2023-01-30 13:35:56 -0800

    Revert "wrap PrometheusBuilder to remove unsupported methods"
    This reverts commit e6be1559f3b46e3ba1773b947ef9e2fce6de1328.

--- kubert/src/admin/metrics.rs
+++ kubert/src/admin/metrics.rs
@@ -1,143 +1,16 @@
 use super::*;
 
 use hyper::header;
-pub use metrics_exporter_prometheus::{BuildError, Matcher};
-use metrics_exporter_prometheus::{PrometheusBuilder as InnerBuilder, PrometheusHandle};
+pub(super) use metrics_exporter_prometheus::PrometheusBuilder;
+use metrics_exporter_prometheus::PrometheusHandle;
 use metrics_process::Collector;
-pub use metrics_util::MetricKindMask;
 
-use std::{fmt, time::Duration};
-
-/// Configures an admin server's [Prometheus] metrics endpoint.
-///
-/// This builder is passed to the [`admin::Builder::with_prometheus`] method to
-/// enable the metrics endpoint.
-///
-/// [Prometheus]: https://prometheus.io/
-/// [`admin::Builder::with_prometheus`]: super::Builder::with_prometheus
-#[derive(Default)]
-#[cfg_attr(docsrs, doc(cfg(all(feature = "admin", feature = "metrics"))))]
-pub struct PrometheusBuilder {
-    inner: InnerBuilder,
-}
+use std::fmt;
 
 #[derive(Clone)]
 pub(super) struct Prometheus {
     metrics: PrometheusHandle,
     process: Collector,
-}
-
-// === impl PrometheusBuilder ===
-
-impl PrometheusBuilder {
-    /// Creates a new [`PrometheusBuilder`].
-    #[must_use]
-    pub fn new() -> Self {
-        Self {
-            inner: InnerBuilder::new(),
-        }
-    }
-
-    /// Sets the quantiles to use when rendering histograms.
-    ///
-    /// Quantiles represent a scale of 0 to 1, where percentiles represent a scale of 1 to 100, so
-    /// a quantile of 0.99 is the 99th percentile, and a quantile of 0.99 is the 99.9th percentile.
-    ///
-    /// Defaults to a hard-coded set of quantiles: 0.0, 0.5, 0.9, 0.95, 0.99, 0.999, and 1.0. This means
-    /// that all histograms will be exposed as Prometheus summaries.
-    ///
-    /// If buckets are set (via [`set_buckets`][Self::set_buckets] or
-    /// [`set_buckets_for_metric`][Self::set_buckets_for_metric]) then all histograms will be exposed
-    /// as summaries instead.
-    ///
-    /// ## Errors
-    ///
-    /// If `quantiles` is empty, an error variant will be thrown.
-    pub fn set_quantiles(self, quantiles: &[f64]) -> Result<Self, BuildError> {
-        self.try_map(|inner| inner.set_quantiles(quantiles))
-    }
-
-    /// Sets the buckets to use when rendering histograms.
-    ///
-    /// Buckets values represent the higher bound of each buckets.  If buckets are set, then all
-    /// histograms will be rendered as true Prometheus histograms, instead of summaries.
-    ///
-    /// ## Errors
-    ///
-    /// If `values` is empty, an error variant will be thrown.
-    pub fn set_buckets(self, values: &[f64]) -> Result<Self, BuildError> {
-        self.try_map(|inner| inner.set_buckets(values))
-    }
-
-    /// Sets the bucket for a specific pattern.
-    ///
-    /// The match pattern can be a full match (equality), prefix match, or suffix match.  The
-    /// matchers are applied in that order if two or more matchers would apply to a single metric.
-    /// That is to say, if a full match and a prefix match applied to a metric, the full match would
-    /// win, and if a prefix match and a suffix match applied to a metric, the prefix match would win.
-    ///
-    /// Buckets values represent the higher bound of each buckets.  If buckets are set, then any
-    /// histograms that match will be rendered as true Prometheus histograms, instead of summaries.
-    ///
-    /// This option changes the observer's output of histogram-type metric into summaries.
-    /// It only affects matching metrics if [`set_buckets`][Self::set_buckets] was not used.
-    ///
-    /// ## Errors
-    ///
-    /// If `values` is empty, an error variant will be thrown.
-    pub fn set_buckets_for_metric(
-        self,
-        matcher: Matcher,
-        values: &[f64],
-    ) -> Result<Self, BuildError> {
-        self.try_map(|inner| inner.set_buckets_for_metric(matcher, values))
-    }
-
-    /// Sets the idle timeout for metrics.
-    ///
-    /// If a metric hasn't been updated within this timeout, it will be removed from the registry
-    /// and in turn removed from the normal scrape output until the metric is emitted again.  This
-    /// behavior is driven by requests to generate rendered output, and so metrics will not be
-    /// removed unless a request has been made recently enough to prune the idle metrics.
-    ///
-    /// Further, the metric kind "mask" configures which metrics will be considered by the idle
-    /// timeout.  If the kind of a metric being considered for idle timeout is not of a kind
-    /// represented by the mask, it will not be affected, even if it would have othered been removed
-    /// for exceeding the idle timeout.
-    ///
-    /// Refer to the documentation for [`MetricKindMask`](metrics_util::MetricKindMask) for more
-    /// information on defining a metric kind mask.
-    #[must_use]
-    pub fn idle_timeout(self, mask: MetricKindMask, timeout: Option<Duration>) -> Self {
-        Self {
-            inner: self.inner.idle_timeout(mask, timeout),
-        }
-    }
-
-    /// Adds a global label to this exporter.
-    ///
-    /// Global labels are applied to all metrics.  Labels defined on the metric key itself have precedence
-    /// over any global labels.  If this method is called multiple times, the latest value for a given label
-    /// key will be used.
-    #[must_use]
-    pub fn add_global_label<K, V>(self, key: K, value: V) -> Self
-    where
-        K: Into<String>,
-        V: Into<String>,
-    {
-        Self {
-            inner: self.inner.add_global_label(key, value),
-        }
-    }
-
-    fn try_map(
-        self,
-        f: impl FnOnce(InnerBuilder) -> Result<InnerBuilder, BuildError>,
-    ) -> Result<Self, BuildError> {
-        Ok(Self {
-            inner: f(self.inner)?,
-        })
-    }
 }
 
 impl Prometheus {
