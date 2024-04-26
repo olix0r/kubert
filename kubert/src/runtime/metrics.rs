d04f3e3a2fd173e4f055dc43a9e6946bdc2e0efd
Oliver Gould <ver@buoyant.io>
2024-04-26 09:39:16 -0700

    chore(runtime)!: unify events metric with a label (#247)

--- kubert/src/runtime/metrics.rs
+++ kubert/src/runtime/metrics.rs
@@ -3,7 +3,7 @@ use futures_util::StreamExt;
 use kube_core::Resource;
 use kube_runtime::watcher;
 use prometheus_client::{
-    encoding::EncodeLabelSet,
+    encoding::{EncodeLabelSet, EncodeLabelValue},
     metrics::{counter::Counter, family::Family},
     registry::Registry,
 };
@@ -12,49 +12,41 @@ use std::fmt::Debug;
 /// Metrics for tracking resource watch events.
 #[derive(Clone, Debug)]
 pub(super) struct ResourceWatchMetrics {
-    watch_applies: Family<ResourceWatchLabels, Counter>,
-    watch_restarts: Family<ResourceWatchLabels, Counter>,
-    watch_deletes: Family<ResourceWatchLabels, Counter>,
-    watch_errors: Family<ResourceWatchErrorLabels, Counter>,
+    watch_events: Family<EventLabels, Counter>,
+    watch_errors: Family<ErrorLabels, Counter>,
 }
 
 #[derive(Clone, Debug, Hash, PartialEq, Eq, EncodeLabelSet)]
-struct ResourceWatchLabels {
+struct EventLabels {
+    op: EventOp,
     kind: String,
     group: String,
     version: String,
 }
 
 #[derive(Clone, Debug, Hash, PartialEq, Eq, EncodeLabelSet)]
-struct ResourceWatchErrorLabels {
+struct ErrorLabels {
     kind: String,
     group: String,
     version: String,
     error: &'static str,
 }
 
+#[derive(Clone, Debug, Hash, PartialEq, Eq, EncodeLabelValue)]
+enum EventOp {
+    Apply,
+    Restart,
+    Delete,
+}
+
 impl ResourceWatchMetrics {
     /// Creates a new set of metrics and registers them.
     pub(super) fn register(registry: &mut Registry) -> Self {
-        let watch_applies = Family::default();
+        let watch_events = Family::default();
         registry.register(
-            "applies",
+            "events",
             "Count of apply events for a resource watch",
-            watch_applies.clone(),
-        );
-
-        let watch_restarts = Family::default();
-        registry.register(
-            "restarts",
-            "Count of restart events for a resource watch",
-            watch_restarts.clone(),
-        );
-
-        let watch_deletes = Family::default();
-        registry.register(
-            "deletes",
-            "Count of delete events for a resource watch",
-            watch_deletes.clone(),
+            watch_events.clone(),
         );
 
         let watch_errors = Family::default();
@@ -65,9 +57,7 @@ impl ResourceWatchMetrics {
         );
 
         Self {
-            watch_applies,
-            watch_restarts,
-            watch_deletes,
+            watch_events,
             watch_errors,
         }
     }
@@ -86,40 +76,52 @@ impl ResourceWatchMetrics {
         let kind = T::kind(&dt).into_owned();
         let group = T::group(&dt).into_owned();
         let version = T::version(&dt).into_owned();
-        let labels = ResourceWatchLabels {
+        let apply_labels = EventLabels {
             kind,
             group,
             version,
+            op: EventOp::Apply,
+        };
+        let restart_labels = EventLabels {
+            op: EventOp::Restart,
+            ..apply_labels.clone()
+        };
+        let delete_labels = EventLabels {
+            op: EventOp::Delete,
+            ..apply_labels.clone()
+        };
+        let error_labels = ErrorLabels {
+            kind: apply_labels.kind.clone(),
+            group: apply_labels.group.clone(),
+            version: apply_labels.version.clone(),
+            error: "", // replaced later
         };
 
         watch.map(move |event| {
             if let Some(metrics) = &metrics {
                 match event {
-                    Ok(watcher::Event::Applied(_)) => {
-                        metrics.watch_applies.get_or_create(&labels).inc();
-                    }
                     Ok(watcher::Event::Restarted(_)) => {
-                        metrics.watch_restarts.get_or_create(&labels).inc();
+                        metrics.watch_events.get_or_create(&restart_labels).inc();
+                    }
+                    Ok(watcher::Event::Applied(_)) => {
+                        metrics.watch_events.get_or_create(&apply_labels).inc();
                     }
                     Ok(watcher::Event::Deleted(_)) => {
-                        metrics.watch_deletes.get_or_create(&labels).inc();
+                        metrics.watch_events.get_or_create(&delete_labels).inc();
                     }
                     Err(ref e) => {
-                        let error = match e {
-                            watcher::Error::InitialListFailed(_) => "InitialListFailed",
-                            watcher::Error::WatchStartFailed(_) => "WatchStartFailed",
-                            watcher::Error::WatchError(_) => "WatchError",
-                            watcher::Error::WatchFailed(_) => "WatchFailed",
-                            watcher::Error::NoResourceVersion => "NoResourceVersion",
-                            watcher::Error::TooManyObjects => "TooManyObjects",
+                        let labels = ErrorLabels {
+                            error: match e {
+                                watcher::Error::InitialListFailed(_) => "InitialListFailed",
+                                watcher::Error::WatchStartFailed(_) => "WatchStartFailed",
+                                watcher::Error::WatchError(_) => "WatchError",
+                                watcher::Error::WatchFailed(_) => "WatchFailed",
+                                watcher::Error::NoResourceVersion => "NoResourceVersion",
+                                watcher::Error::TooManyObjects => "TooManyObjects",
+                            },
+                            ..error_labels.clone()
                         };
-                        let error_labels = ResourceWatchErrorLabels {
-                            kind: labels.kind.clone(),
-                            group: labels.group.clone(),
-                            version: labels.version.clone(),
-                            error,
-                        };
-                        metrics.watch_errors.get_or_create(&error_labels).inc();
+                        metrics.watch_errors.get_or_create(&labels).inc();
                     }
                 };
             }
